097fbe750337c3890d5289e2087ea09bc27e97ac
Arturo Quirós <joseq2408@gmail.com>
2022-08-13 14:54:25 -0600

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +19,22 @@ const Home: NextPage = () => {
         <Typography variant="h2" component={"h1"} sx={{ mb: 1 }}>
           Todos los productos
         </Typography>
+
+        <Grid container spacing={4}>
+          {initialData.products.map((product) => (
+            <Grid key={product.slug} item xs={6} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component={"img"}
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  ></CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </ShopLayout>
     </>
   );
