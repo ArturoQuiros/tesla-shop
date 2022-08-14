30da29b74951d2b4cb84cf35b9586de7f3a5c426
Arturo Quirós <joseq2408@gmail.com>
2022-08-13 18:44:43 -0600

    cards

---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { FC, useMemo, useState } from "react";
+import { IProduct } from "../../interfaces";
+
+interface Props {
+  product: IProduct;
+}
+
+export const ProductCard: FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
+  return (
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component={"img"}
+            className="fadeIn"
+            image={productImage}
+            alt={product.title}
+          ></CardMedia>
+        </CardActionArea>
+      </Card>
+
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+      </Box>
+    </Grid>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
+import { ProductList } from "../components/products";
 import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
@@ -20,21 +15,7 @@ const Home: NextPage = () => {
           Todos los productos
         </Typography>
 
-        <Grid container spacing={4}>
-          {initialData.products.map((product) => (
-            <Grid key={product.slug} item xs={6} sm={4}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component={"img"}
-                    image={`products/${product.images[0]}`}
-                    alt={product.title}
-                  ></CardMedia>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+        <ProductList products={initialData.products as any}></ProductList>
       </ShopLayout>
     </>
   );
