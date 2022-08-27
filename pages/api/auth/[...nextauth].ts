d3651c237bed3ec41e7c97ca7dc046a083df28f5
Arturo Quirós <joseq2408@gmail.com>
2022-08-26 19:31:15 -0600

    nextauth 3

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,36 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { ArchitectureOutlined } from "@mui/icons-material";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      name: "Custom Login",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "youremail@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        return {
+          name: "arturo",
+          email: "asda@asd.ca",
+          role: "admin",
+        };
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
     // ...add more providers here
   ],
+
+  //*Callbacks
+  callbacks: {},
 });
