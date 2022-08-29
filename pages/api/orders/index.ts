c1ed0d06ccaa9b4cb251474da12ae4196b0873e5
Arturo Quirós <joseq2408@gmail.com>
2022-08-28 20:48:02 -0600

    x

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,4 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { db } from "../../../database";
+import { IOrder } from "../../../interfaces/order";
+import { Product } from "../../../models";
 
 type Data = {
   message: string;
@@ -17,8 +21,35 @@ export default function handler(
   }
 }
 
-const createOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const body = req.body;
+const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { orderItems, total } = req.body as IOrder;
 
-  return res.status(201).json(body);
+  //*verificar usuario
+  const session: any = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ message: "Unathorized" });
+  }
+
+  //*arreglo de productos
+  const productsIds = orderItems.map((p) => p._id);
+  await db.connect();
+  const dbProducts = await Product.find({ _id: { $in: productsIds } });
+
+  try {
+    const subTotal = orderItems.reduce((prev, current) => {
+      const currentPrice = dbProducts.find(
+        (prod) => prod._id === current._id
+      )?.price;
+
+      if (!currentPrice) {
+        throw new Error("Check the cart, no products exists");
+      }
+
+      return current.price * current.quantity + prev;
+    }, 0);
+  } catch (error) {}
+
+  return res.status(201).json(req.body);
+  //!
 };
