8b048358a7f0ad574469bd7721a698dc75034275
Arturo Quirós <joseq2408@gmail.com>
2022-08-29 18:04:16 -0600

    orders created

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { db } from "../../../database";
 import { IOrder } from "../../../interfaces/order";
-import { Product } from "../../../models";
+import { Order, Product } from "../../../models";
 
-type Data = {
-  message: string;
-};
+type Data =
+  | {
+      message: string;
+    }
+  | IOrder;
 
 export default function handler(
   req: NextApiRequest,
@@ -37,9 +39,10 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const dbProducts = await Product.find({ _id: { $in: productsIds } });
 
   try {
+    //*calculamos el costos de la orden en el backend
     const subTotal = orderItems.reduce((prev, current) => {
       const currentPrice = dbProducts.find(
-        (prod) => prod._id === current._id
+        (prod) => prod.id === current._id
       )?.price;
 
       if (!currentPrice) {
@@ -48,8 +51,26 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
       return current.price * current.quantity + prev;
     }, 0);
-  } catch (error) {}
 
-  return res.status(201).json(req.body);
-  //!
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+
+    const backendTotal = subTotal * (taxRate + 1);
+
+    if (total !== backendTotal) {
+      throw new Error("Frontend and Backend values does not match");
+    }
+    //*los montos SI coinciden
+    const userId = session.user._id;
+    const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+    await newOrder.save();
+
+    return res.status(201).json(newOrder);
+  } catch (error: any) {
+    //! Los montos no son compatibles
+    await db.disconnect();
+    console.log(error);
+    return res
+      .status(400)
+      .json({ message: error.message || "Invalid request, check the log" });
+  }
 };
